23b98af6adea1f8a0477f95d7ebe3e4c8dae1700
Jed Davis <jld@mozilla.com>
2013-03-28 14:32:20 -0700

    Resolve mmaps in perf-to-sps, and give up on multi-space symbolicate (for now).

--- scripts/profile-symbolicate.py
+++ scripts/profile-symbolicate.py
@@ -27,7 +27,6 @@ class Library:
     self.end = lib_dict["end"]
     self.offset = lib_dict["offset"]
     self.target_name = lib_dict["name"]
-    self.space = lib_dict.get("space", None)
     self.verbose = verbose
     self.host_name = None
     self.located = False
@@ -173,8 +172,7 @@ class Library:
     for i in range(0,len(addresses_strs), 256):
       slice = addresses_strs[i:i+256]
       if progress:
-        print "Resolving symbols for", self.target_name, \
-            ("(space %s)" % self.space if self.space else ""), len(slice), "addresses"
+        print "Resolving symbols for", self.target_name, len(slice), "addresses"
       syms = self.AddressesToSymbols(slice)
       for j in range(len(syms)):
         self.symbols[addresses_strs[i+j]] = syms[j]
@@ -206,24 +204,20 @@ class Libraries:
     for lib in self.libs:
       lib.DumpSymbols()
 
-  def AddressToLib(self, address, space):
+  def AddressToLib(self, address):
     """Does a binary search through our ordered collection of libraries."""
-    limit = bisect.bisect(self.libs_start, address)
-    if limit == 0:
-      return None
-    for i in reversed(xrange(limit)):
+    i = bisect.bisect(self.libs_start, address)
+    if i:
+      i = i - 1
+    if i < len(self.libs_start):
       lib = self.libs[i]
-      if lib.space != space:
-        continue
-      return lib if lib.ContainsAddress(address) else None
-    return None
+      if lib.ContainsAddress(address):
+        return lib
 
-  def Lookup(self, address, space):
+  def Lookup(self, address):
     """Figures out which library a given address comes from."""
-    if not (self.last_lib and
-            self.last_lib.ContainsAddress(address) and
-            self.last_lib.space == space):
-      self.last_lib = self.AddressToLib(address, space)
+    if not (self.last_lib and self.last_lib.ContainsAddress(address)):
+      self.last_lib = self.AddressToLib(address)
     return self.last_lib
 
   def ResolveSymbols(self, progress=True):
@@ -247,7 +241,7 @@ class Libraries:
             # Quick optimization since lots of times the same address appears
             # many times in a row. We only need to add each address once.
             if address != last_address:
-              lib = self.Lookup(address, sample.get("space", None))
+              lib = self.Lookup(address)
               if lib:
                 lib.AddUnresolvedAddress(address)
                 last_address = address
@@ -308,7 +302,7 @@ def main():
   if args.lookup:
     address_str = args.lookup
     address = int(address_str, 0)
-    lib = libs.Lookup(address, None)
+    lib = libs.Lookup(address)
     if lib:
       lib.Locate()
       print("Address 0x%08x maps to symbol '%s'" % (address, lib.AddressToSymbol(address_str)))
